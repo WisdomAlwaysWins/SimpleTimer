61d938076aa08c3f395aafaf245ded978cacec53
J <knowisdom314@gmail.com>
2025-03-31 16:37:43 +0900

    Initial Commit

---
+++ timer-basic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timer-basic
+//
+//  Created by J on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timer-basic/timer_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  timer_basicApp.swift
+//  timer-basic
+//
+//  Created by J on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct timer_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
